4812b1df2d1d795f836e20616fed8bad87573ca2
Zahangir Alam <eng.zahangir20bd@gmail.com>
2023-07-24 16:45:15 +0600

    Update User Profile Successful

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const port = process.env.PORT || 5000;
 
@@ -42,6 +42,25 @@ async function run() {
         return res.send({ message: "User already exists" });
       }
       const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
+    app.get("/users", async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.put("/users/:id", async (req, res) => {
+      const _id = req.params.id;
+      const updateProfile = req.body;
+      console.log("Update", updateProfile);
+
+      const result = await usersCollection.updateOne(
+        {
+          _id: new ObjectId(_id),
+        },
+        { $set: updateProfile }
+      );
       res.send(result);
     });
 
